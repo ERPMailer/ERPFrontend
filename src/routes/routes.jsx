0244dbbacd3e77dab15a6d255a83c6562788e34e
VishalLende24 <vishallende24.com>
2025-05-24 12:27:43 +0530

    Routes by module

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,12 @@
 import Home from "../components/home";
+import { campaignRouets } from "./campaignRoutes";
+import { dashboardRouets } from "./dashboardRoutes";
+import { tepmplateRouets } from "./templateRoute";
 
 export const routes = [
+  ...campaignRouets,
+  ...dashboardRouets,
+  ...tepmplateRouets,
   {
     path: "/",
     element: <Home />,
