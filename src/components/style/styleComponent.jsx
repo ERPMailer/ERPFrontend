ddce19cbc1ad5fc03e5c6b7d43767b9fa88012f6
VishalLende24 <vishallende24.com>
2025-05-24 12:35:29 +0530

    [UPDATE] : npm packages added

---
+++ src/components/style/styleComponent.jsx
@@ -0,0 +1,9 @@
+import { Card, styled } from "@mui/material";
+
+export const MainCard = styled(Card)`
+
+  padding: 16px;
+  background-color: #ffffff;
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+`;
